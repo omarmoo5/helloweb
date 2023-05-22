7a7a6cad6c5140850eaca240b8735179dcae9a07
Omar Mokhtar <omokhtarm@gmail.com>
2023-05-22 21:14:23 +0300

    import custom repo

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import flwr as fl
 
 def run_python_code():
+    st.write(fl.server.strategy.__all__)
     # Your Python code goes here
     st.write("Hello, Streamlit!")
 
--- setup.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/omarmoo5/flower-secagg.git
-cd flower-segagg
-pip install -e .
